f24432c3181a0e4f1d1b1cddeac1b69d740444c8
tamir.rust <timorust1@gmail.com>
2023-11-30 14:59:13 +0200

    trpc

--- back/server/trpc/index.ts
+++ back/server/trpc/index.ts
@@ -1,4 +1,8 @@
-import { router } from "./trpc";
-const appRouter = router({});
+import { publicProcedure, router } from "./trpc";
+const appRouter = router({
+  posts: publicProcedure.query(async () => {
+    return [10, 20, 30];
+  }),
+});
 
 export type AppRouter = typeof appRouter;
