7fb2b13d399f7a9d53ee09663bf851a46c8e79eb
tamir.rust <timorust1@gmail.com>
2023-12-20 16:36:18 +0200

    add person page and person details

--- front/client/src/routers/PersonPage/PersonDetails.tsx
+++ front/client/src/routers/PersonPage/PersonDetails.tsx
@@ -1,3 +1,19 @@
+import { useParams } from "react-router-dom";
+import { trpc } from "../../trpc";
+
 export function PersonDetails() {
-  return <main>Person details!</main>;
+  const { id } = useParams();
+  const person_query = trpc.person.personDetails.useQuery(id ?? "");
+  return (
+    <main>
+      <h1>Person details!</h1>
+      <h2>{person_query.data?.name}</h2>
+      <h2>{person_query.data?.bio}</h2>
+      {person_query.data?.homes.map((home, index) => (
+        <div key={index}>
+          address = {home.address} -- rooms = {home.rooms}
+        </div>
+      ))}
+    </main>
+  );
 }
--- front/client/src/routers/PersonPage/PersonPage.tsx
+++ front/client/src/routers/PersonPage/PersonPage.tsx
@@ -1,3 +1,18 @@
+import { NavLink } from "react-router-dom";
+import { trpc } from "../../trpc";
+
 export function PersonPage() {
-  return <main>Person page!</main>;
+  const person_list_query = trpc.person.personList.useQuery();
+  if (person_list_query.isLoading) return <div>Loading...</div>;
+  return (
+    <main>
+      <h1>Person page!</h1>
+      {person_list_query.data?.map((person, index) => (
+        <NavLink to={`/persons/${person.id}`} key={index}>
+          <h3>{person.name}</h3>
+          <p>{person.bio}</p>
+        </NavLink>
+      ))}
+    </main>
+  );
 }
